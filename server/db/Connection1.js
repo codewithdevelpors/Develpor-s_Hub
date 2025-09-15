28d687b93243797ae8161acc03150a83bdd65b3f
codewithdevelpors <codewithdevelpors@gmail.com>
2025-09-15 20:45:19 +0500

    Back-end

--- server/db/Connection1.js
+++ server/db/Connection1.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectWithMongoDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(process.env.MONGO_URL)
+        const connectionInstance = await mongoose.connect('mongodb+srv://codewithdevelpors:Qaimpur828@develpors-hub.zuflo2i.mongodb.net/?retryWrites=true&w=majority&appName=Develpors-hub')
         console.log("Mongo connection successfull!!...on host : " + connectionInstance.connection.host)
     } catch (error) {
         console.error("db :: connection :: error : " + error)
