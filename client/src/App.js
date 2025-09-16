732968604b937422d247dbd5d83417ff1fbfc941
codewithdevelpors <codewithdevelpors@gmail.com>
2025-09-16 10:41:57 +0500

    Front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import './styles/App.css';
-import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import Home from './pages/Home';
 import Detail from './pages/Detail';
@@ -9,22 +8,9 @@ import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 
 function AppContent() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
   const [selectedItem, setSelectedItem] = useState(null);
   const [previewItem, setPreviewItem] = useState(null);
   const navigate = useNavigate();
-
-  useEffect(() => {
-    if (isDarkMode) {
-      document.body.classList.add('dark-mode');
-    } else {
-      document.body.classList.remove('dark-mode');
-    }
-  }, [isDarkMode]);
-
-  const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode);
-  };
 
   const handleItemClick = (item) => {
     setSelectedItem(item);
@@ -46,7 +32,6 @@ function AppContent() {
 
   return (
     <div className="App">
-      <Navbar toggleDarkMode={toggleDarkMode} isDarkMode={isDarkMode} />
       <Routes>
         <Route path="/" element={<Home onItemClick={handleItemClick} />} />
         <Route path="/detail/:id" element={
